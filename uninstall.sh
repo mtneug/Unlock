21793a3189750df390a9ea83fd901fafb06b6601
Delfim Machado <dbcm@profundos.org>
2011-08-18 21:14:09 +0100

    update path names

--- uninstall.sh
+++ uninstall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "Uninstalling..."
-cmd() { sudo security 2>&1 >/dev/null delete-generic-password -D "Encrypted Volume Password" -s "name.ridgewell.unlock" "/Library/Keychains/System.keychain"; }
+cmd() { sudo security 2>&1 >/dev/null delete-generic-password -D "Encrypted Volume Password" -s "UnlockLogicalVolumeHelper" "/Library/Keychains/System.keychain"; }
 err=`cmd`
 while [[ $err == "password has been deleted." ]]; do
 	err=`cmd`
 done
-sudo rm /Library/LaunchDaemons/name.ridgewell.unlock.plist
-sudo rm /Library/LaunchDaemons/name.ridgewell.unlock
+sudo rm /Library/LaunchDaemons/name.ridgewell.ulvh.plist
+sudo rm -rf /Library/UnlockLogicalVolumeHelper
 
 echo "--------------------------"
 echo ""
